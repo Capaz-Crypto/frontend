6a3a58d9961070040f318e351a40f4e0c7914c88
Bertrand Rigal <bertrandrigal.pro@gmail.com>
2022-10-30 10:58:11 +0000

    🚧 Add wip popup validation message

--- src/components/SendPayment.tsx
+++ src/components/SendPayment.tsx
@@ -26,6 +26,18 @@ export default function SendPayment() {
   const [receiverAddress, setReceiverAddress] = useState(
     '0x0000000000000000000000000000000000000000',
   );
+  const [openModal1, setOpenModal1] = useState(true);
+  const stylesModal1 = {
+    popup: {
+      display: openModal1 ? 'block' : 'none',
+    },
+  };
+  const [openModal2, setOpenModal2] = useState(false);
+  const stylesModal2 = {
+    popup: {
+      display: openModal2 ? 'block' : 'none',
+    },
+  };
 
   const getAmountFromEthInWei = ethAmount =>
     ethers.utils.parseUnits(String(ethAmount), selectedToken?.decimals).toString();
@@ -69,10 +81,13 @@ export default function SendPayment() {
     if (approveTxHasLoaded !== true) {
       onApprove().then(() => {
         setApproveTxHasLoaded(true);
+        setOpenModal1(true);
       });
     } else {
       onPayment().then(() => {
         setExecuteTxHasLoaded(true);
+        setOpenModal1(false);
+        setOpenModal2(true);
       });
     }
   }
@@ -364,20 +379,55 @@ export default function SendPayment() {
                   {executeTx.error.message.split(' (')[0]}
                 </div>
               )}
-              {/* display info message */}
-              {approveTxHasLoaded && !executeTxHasLoaded && (
-                <div className='text-blue-500 text-sm font-semibold'>
-                  You confirmed the transaction, you need to execute it by clicking on "Send Payment".
-                </div>
-              )}
-              {/* display success message */}
-              {executeTxHasLoaded && (
-                <div className='text-green-500 text-sm font-semibold'>
-                  Bravo, your transaction is confirmed
-                </div>
-              )}
             </div>
           </form>
+          {/* display info message */}
+          <div className='z-50 fixed left-0 bottom-0 p-4' style={stylesModal1.popup}>
+            <div className='max-w-xs p-4 bg-gray-500 rounded-lg shadow'>
+              <div className='flex w-full h-full items-center justify-between'>
+                <div className='pr-6'>
+                  <span className='text-sm leading-5 text-gray-200 font-semibold'>
+                    You confirmed the transaction, you need to execute it by clicking on "Send
+                    Payment".
+                  </span>
+                </div>
+                <button
+                  className='inline-block text-gray-400 hover:text-gray-300'
+                  onClick={() => {
+                    setOpenModal1(false);
+                  }}>
+                  <svg width='10' height='10' viewBox='0 0 10 10' fill='none'>
+                    <path
+                      d='M5.93996 5.00001L8.80663 2.14001C8.93216 2.01448 9.00269 1.84421 9.00269 1.66668C9.00269 1.48914 8.93216 1.31888 8.80663 1.19335C8.68109 1.06781 8.51083 0.997284 8.33329 0.997284C8.15576 0.997284 7.9855 1.06781 7.85996 1.19335L4.99996 4.06001L2.13996 1.19335C2.01442 1.06781 1.84416 0.997284 1.66663 0.997284C1.48909 0.997284 1.31883 1.06781 1.19329 1.19335C1.06776 1.31888 0.997231 1.48914 0.997231 1.66668C0.997231 1.84421 1.06776 2.01448 1.19329 2.14001L4.05996 5.00001L1.19329 7.86001C1.13081 7.92199 1.08121 7.99572 1.04737 8.07696C1.01352 8.1582 0.996094 8.24534 0.996094 8.33334C0.996094 8.42135 1.01352 8.50849 1.04737 8.58973C1.08121 8.67097 1.13081 8.7447 1.19329 8.80668C1.25527 8.86916 1.329 8.91876 1.41024 8.95261C1.49148 8.98645 1.57862 9.00388 1.66663 9.00388C1.75463 9.00388 1.84177 8.98645 1.92301 8.95261C2.00425 8.91876 2.07798 8.86916 2.13996 8.80668L4.99996 5.94001L7.85996 8.80668C7.92194 8.86916 7.99567 8.91876 8.07691 8.95261C8.15815 8.98645 8.24529 9.00388 8.33329 9.00388C8.4213 9.00388 8.50844 8.98645 8.58968 8.95261C8.67092 8.91876 8.74465 8.86916 8.80663 8.80668C8.86911 8.7447 8.91871 8.67097 8.95255 8.58973C8.9864 8.50849 9.00383 8.42135 9.00383 8.33334C9.00383 8.24534 8.9864 8.1582 8.95255 8.07696C8.91871 7.99572 8.86911 7.92199 8.80663 7.86001L5.93996 5.00001Z'
+                      fill='currentColor'></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* display success message */}
+          <div className='z-50 fixed left-0 bottom-0 p-4' style={stylesModal2.popup}>
+            <div className='max-w-xs p-4 bg-gray-500 rounded-lg shadow'>
+              <div className='flex w-full h-full items-center justify-between'>
+                <div className='pr-6'>
+                  <span className='text-sm leading-5 text-gray-200 font-semibold'>
+                    Bravo ! Your transaction is confirmed
+                  </span>
+                </div>
+                <button
+                  className='inline-block text-gray-400 hover:text-gray-300'
+                  onClick={() => {
+                    setOpenModal2(false);
+                  }}>
+                  <svg width='10' height='10' viewBox='0 0 10 10' fill='none'>
+                    <path
+                      d='M5.93996 5.00001L8.80663 2.14001C8.93216 2.01448 9.00269 1.84421 9.00269 1.66668C9.00269 1.48914 8.93216 1.31888 8.80663 1.19335C8.68109 1.06781 8.51083 0.997284 8.33329 0.997284C8.15576 0.997284 7.9855 1.06781 7.85996 1.19335L4.99996 4.06001L2.13996 1.19335C2.01442 1.06781 1.84416 0.997284 1.66663 0.997284C1.48909 0.997284 1.31883 1.06781 1.19329 1.19335C1.06776 1.31888 0.997231 1.48914 0.997231 1.66668C0.997231 1.84421 1.06776 2.01448 1.19329 2.14001L4.05996 5.00001L1.19329 7.86001C1.13081 7.92199 1.08121 7.99572 1.04737 8.07696C1.01352 8.1582 0.996094 8.24534 0.996094 8.33334C0.996094 8.42135 1.01352 8.50849 1.04737 8.58973C1.08121 8.67097 1.13081 8.7447 1.19329 8.80668C1.25527 8.86916 1.329 8.91876 1.41024 8.95261C1.49148 8.98645 1.57862 9.00388 1.66663 9.00388C1.75463 9.00388 1.84177 8.98645 1.92301 8.95261C2.00425 8.91876 2.07798 8.86916 2.13996 8.80668L4.99996 5.94001L7.85996 8.80668C7.92194 8.86916 7.99567 8.91876 8.07691 8.95261C8.15815 8.98645 8.24529 9.00388 8.33329 9.00388C8.4213 9.00388 8.50844 8.98645 8.58968 8.95261C8.67092 8.91876 8.74465 8.86916 8.80663 8.80668C8.86911 8.7447 8.91871 8.67097 8.95255 8.58973C8.9864 8.50849 9.00383 8.42135 9.00383 8.33334C9.00383 8.24534 8.9864 8.1582 8.95255 8.07696C8.91871 7.99572 8.86911 7.92199 8.80663 7.86001L5.93996 5.00001Z'
+                      fill='currentColor'></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
